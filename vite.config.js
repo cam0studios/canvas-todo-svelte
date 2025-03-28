6a96aec76999371671eb8a3dadc4985c54b2b745
cam0studios <cam321blastoff@gmail.com>
2025-03-28 14:07:29 -0400

    fix: subdomain appearing when in dev mode

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,20 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		svelte(),
-		tailwindcss()
-	],
-	base: "/canvas-todo-svelte/"
-})
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+	if (command == "serve") {
+		return {
+			plugins: [
+				svelte(),
+				tailwindcss()
+			]
+		}
+	}
+	return {
+		plugins: [
+			svelte(),
+			tailwindcss()
+		],
+		base: "/canvas-todo-svelte/"
+	}
+});
